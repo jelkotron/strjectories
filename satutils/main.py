ef5c1373eb0e6d665ac35dcea2a5b0e8e0e96280
schnollie <ojelko@proton.me>
2024-07-23 20:32:17 +0200

    implements keyframe interpolation

--- satutils/main.py
+++ satutils/main.py
@@ -1,106 +1,51 @@
 #!/home/schnollie/.venvs/strwueue/bin/python
-from map import live_plotter, map_plotter
-import requests
-import numpy
-import json
 import time 
 import os
-
-from skyfield.api import Distance, load, wgs84, EarthSatellite
-from skyfield.positionlib import Geocentric
-
 from satcat import SatCat, Sat
 
-# def tle2latlonhgt(line1, line2, id, timescale, to_degrees=True):
-#     satellite = EarthSatellite(line1, line2, id, timescale)
-#     geocentric = satellite.at(timescale.now())
-#     lat, lon = wgs84.latlon_of(geocentric)
-#     hgt = wgs84.height_of(geocentric)
+# from map import live_plotter, map_plotter
+# import requests
+# import numpy
+# import json
 
-#     if to_degrees:
-#         lat = lat.degrees
-#         lon = lon.degrees
+# from skyfield.api import Distance, load, wgs84, EarthSatellite
+# from skyfield.positionlib import Geocentric
+SATCATPATH = '/tmp/satlist.json'
+DATAURL = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=active'
+MAX_SATELLITES = 1
 
-#     return lat, lon, hgt
+MINUTE = 60
+HOUR = 3600
+DAY = 86400
 
-# def fetch_satellites(url, file='/tmp/satlist.json'):
-#     t0 = time.perf_counter()
-#     timestamp = time.gmtime()
-#     data = {
-#         'timestamp' : timestamp, 
-#         'satellites' : {}
-#     }
-#     r = requests.get(url)
-#     text = r.text.split("\n")
 
-#     for i in range(len(text)-2):
-#         line = text[i]
-#         id = None
-#         tle_1 = None
-#         tle_2 = None
+def main(file=None, url=None):
+    if not url:
+        url = DATAURL
+     
+    if not file:
+        file = SATCATPATH
+        if not os.path.isfile(file):
+            catalog = SatCat(file)
+            print("Info: Retrieving Data from %s"%url)
+            catalog.fetch(url)
+            print("Info: Download successful")
+            
+            # TODO: Execption Handling; update file
+        else:
+            print("Info: Reading Data from %s"%file)
 
-#         if not line.startswith("1") and not line.startswith("2"):
-#             id = line.strip()
-#             tle_1 = text[i+1].strip()
-#             tle_2 = text[i+2].strip()
-
-#         if id and tle_1 and tle_2:
-#             ts = load.timescale()
-#             lat, lon, hgt = tle2latlonhgt(tle_1, tle_2, id, ts)
-
-#             data['satellites'][id] = {
-#                 'tle': '\n'.join([tle_1, tle_2]),
-#                 'lat': lat,
-#                 'lon': lon,
-#                 'height': hgt,
-#                 }
+    catalog = SatCat(file)
+    data = catalog.read()
+    catalog.create_keyframes(10, HOUR)
+    catalog.interpolate_keyframes(10)
+    
+    for i in range(len(catalog.objects)):
+        obj = catalog.objects[i]
+        obj.update_position()
+    
+ 
             
 
-#     with open(file, 'w', encoding='utf-8') as f:
-#         json.dump(data, f, ensure_ascii=False, indent=4)
 
-#     t1 = time.perf_counter()
-#     print("Fetch: Finished in %f seconds"%(t1-t0))
-
-
-
-# def read_satellites(file):
-#     t0 = time.perf_counter()
-#     ts = load.timescale() # TODO: unify!
-#     t = ts.utc(2014, 1, 23, 11, 18, 7)
-#     with open(file, 'r') as f:
-#         d = json.load(f)
-        
-#         for key, value in d["satellites"].items():
-#             tle = value['tle'].split('\n')
-           
-#             print('id:', key)
-#             print('Latitude:', value["lat"])
-#             print('Longitude:', value["lon"])
-    
-#     t1 = time.perf_counter()
-#     perf = t1 - t0
-#     print("Read: Finished in %f seconds."%perf)
-
-
-url = 'https://celestrak.org/NORAD/elements/gp.php?GROUP=active'
-file = '/tmp/satlist.json'
-
-catalog = SatCat(file)
-d = catalog.read()
-num = 0
-MAX_SATELLITES = 100000
-
-t_perf0 = time.perf_counter()
-
-for id, info in d["satellites"].items():
-    if num < MAX_SATELLITES:
-        sat = Sat(id, info['tle'])
-        sat.get_keyframes(None, 1, 60)
-        for k in sat.keyframes:
-            print(k)
-        num += 1
-
-t_perf1 = time.perf_counter()
-d_t_perf = t_perf1 - t_perf0
-print("Animation: Finished in %f seconds"%d_t_perf)
+main()
--- satutils/satcat.py
+++ satutils/satcat.py
@@ -2,9 +2,12 @@
 import json
 import time 
 import requests
-from skyfield.api import Distance, load, wgs84, EarthSatellite
+from skyfield.api import Distance, load, wgs84, EarthSatellite, utc
 from skyfield.positionlib import Geocentric
+from datetime import timedelta, datetime, timezone
 
+MAX_SATELLITES = 1
+TIMEFORMAT = '%Y-%m-%d %H:%M:%S'
 
 def tle2latlonhgt(line1, line2, id, timescale, time, to_degrees=True):
     satellite = EarthSatellite(line1, line2, id, timescale)
@@ -20,40 +23,96 @@ def tle2latlonhgt(line1, line2, id, timescale, time, to_degrees=True):
 
 
 class Sat(object):
-    def __init__(self, id, tle):
+    def __init__(self, id, tle, timescale, t0):
         self.id = id
-        self.tle = tle
+        self.tle = tle.split('\n')
         self.keyframes = []
+        self.timescale = timescale
+        self.t0 = t0
+        self.lat, self.lon, self.hgt = 0, 0, 0
+
+    def create_keyframes(self, num_frames, frame_time):
+        years, months, days, hours, minutes, seconds = self.t0
+        steps = self.timescale.utc(years, months, days, hours, minutes, range(0, num_frames*frame_time, frame_time))
+
+        for i in range(len(steps)):
+            time = steps[i]
+            kf = tle2latlonhgt(self.tle[0], self.tle[1], self.id, self.timescale, time)
+            self.keyframes.append([time.utc_strftime(TIMEFORMAT), kf])
+
+        self.lat = self.keyframes[0][1][0]
+        self.lon = self.keyframes[0][1][1]
+        self.hgt = self.keyframes[0][1][2]
+
+    def interpolate_keyframes(self, num_substeps):
+        substeps = []
+        for i in range(len(self.keyframes)):
+            if i > 0:
+                for j in range(num_substeps):
+                    kf = self.keyframes[i]
+                    t = datetime.strptime(kf[0], TIMEFORMAT)
+
+                    kf_previous = self.keyframes[i-1]
+                    t_previous = datetime.strptime(kf_previous[0], TIMEFORMAT)
+  
+                    secs = (t - t_previous) / (num_substeps+1) * (j+1)
+                    d_time = t_previous + timedelta(seconds = secs.total_seconds())
+
+                    d_lat = kf_previous[1][0] + (kf[1][0] - kf_previous[1][0]) / num_substeps * j
+                    d_lon = kf_previous[1][1] + (kf[1][1] - kf_previous[1][1]) / num_substeps * j
+                    d_hgt = kf_previous[1][2] + (kf[1][2] - kf_previous[1][2]) / num_substeps * j
+                    
+                    substeps.append(
+                        [d_time.strftime(TIMEFORMAT), (d_lat, d_lon, d_hgt)]
+                    )
+
+            substeps.append(self.keyframes[i])
+                    
+        self.keyframes = substeps
+
+    def update_position(self):
+        t_now = self.timescale.now().utc
+        num_rm = 0
+        if len(self.keyframes) > 0:
+            t_key = datetime.strptime(self.keyframes[0][0], TIMEFORMAT).replace(tzinfo=utc)
+            t_key = self.timescale.utc(t_key).utc
+            while t_key < t_now:
+                try:
+                    self.keyframes.pop(0)
+                    num_rm += 1
+                except IndexError:
+                    pass
         
+        if num_rm > 0:
+            print("Info: Removed %i obsolete keyframes."%num_rm)
 
-    def get_keyframes(self, t0, frame_time, num_frames):
-        ts = load.timescale()
-        utc_time = ts.now().utc
-        years = utc_time[0]
-        months = utc_time[1]
-        days = utc_time[2]
-        hours = utc_time[3]
-        minutes = utc_time[4]
-        seconds = utc_time[5] 
-        
-        l1 = self.tle.split('\n')[0]
-        l2 = self.tle.split('\n')[1]
-        
-        steps = ts.utc(years, months, days, hours, minutes, range(num_frames))
-        
-        for time in steps:
-            kf = tle2latlonhgt(l1, l2, self.id, ts, time)
-            self.keyframes.append({time: kf})
+        if len(self.keyframes) > 0:
+            key = self.keyframes.pop(0)
+            self.lat, self.lon, self.hgt = key[1]
 
+        # for i in range(len(self.keyframes)):
+        #     key = self.keyframes[i]
+        #     t_key = datetime.strptime(key[0], TIMEFORMAT).replace(tzinfo=utc)
+        #     t_key = self.timescale.utc(t_key).utc
+            
+        #     if t_key < t_now:
 
+            # print(t_key)
+            # print(type(t_key))
+            # print("################################")
+            # print("-->", t_now)
+            # print(type(t_now))
+            # print("################################")
 
-
+            
 
 class SatCat(object):
     def __init__(self, file):
         self.file = file
+        self.num_objects = 0
         self.objects = []
-
+        self.timescale = load.timescale()
+        self.t0 = self.timescale.now().utc
 
     def fetch(self, url):
         t0 = time.perf_counter()
@@ -100,25 +159,46 @@ class SatCat(object):
                 json.dump(data, f, ensure_ascii=False, indent=4)
 
 
-
     def read(self, file=None):
         t0 = time.perf_counter()
+        
         if not file:
             file = self.file
 
         if file:
-
-            ts = load.timescale() # TODO: unify!
-            d = None
+            ts = load.timescale()
+            data = None
             with open(file, 'r') as f:
-                d = json.load(f)
-                
-            
+                data = json.load(f)
+
+        for id, info in data["satellites"].items():
+            sat = Sat(id, info['tle'], self.timescale, self.t0)
+            self.objects.append(sat) # TODO: Evaluate data type
+            self.num_objects += 1
+
         t1 = time.perf_counter()
         perf = t1 - t0
         print("Read: Finished in %f seconds."%perf)
-        
-        return d
+        return data
+
+
+    def create_keyframes(self, num_frames, frame_time):
+        for i in range(MAX_SATELLITES):
+            if i < len(self.objects):
+                sat = self.objects[i]
+                sat.create_keyframes(num_frames, frame_time)
+                
+    
+    def interpolate_keyframes(self,num_substeps):
+        for sat in self.objects:
+            sat.interpolate_keyframes(num_substeps)
+
+
+    def print_keyframes(self):
+        for sat in self.objects:
+            for k in sat.keyframes:
+                print(k)
+
 
     def update(self, file):
         pass
