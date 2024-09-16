bcd851b34e8efd0751c5fed938863f2f22fa4a9a
schnollie <ojelko@proton.me>
2024-09-16 20:21:06 +0200

    implements (debian) distribution independent laucher

--- launcher.sh
+++ launcher.sh
@@ -2,9 +2,10 @@
 
 basedir=$(dirname $(realpath $0))
 scriptdir=scripts
-main=main.py
+main=$basedir/$scriptdir/main.py
 venv=$basedir/venv/bin/activate
 geo=87x13+0+1025
 title="STRWHAT"
 
-mate-terminal --geometry $geo --title $title -x sh -c ". $venv && python3 $basedir/$scriptdir/$main; bash"
+# mate-terminal --geometry $geo --title $title -x sh -c ". $venv && python3 $basedir/$scriptdir/$main; bash"
+x-terminal-emulator -title $title -geometry $geo -e 'sh -c ". '$venv' && python3 '$main'; bash";'   
